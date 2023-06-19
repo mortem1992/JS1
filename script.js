1f506fc55ac4193deccab9188b67563856ae7ab2
Mortem <gurin.ivan@icloud.com>
2023-06-19 22:35:09 +0500

    JS1

---
+++ script.js
@@ -0,0 +1,15 @@
+var userName = prompt('введите свое имя')
+var age = +prompt('введите свой возраст')
+var primery1 = +prompt('6+3=?')
+var primery2 = +prompt('6-3=?')
+var primery3 = +prompt('6*3=?')
+var primery4 = +prompt('6/3=?')
+var primery5 = +prompt('6%3=?')
+console.log(userName);
+console.log(age)
+console.log('6+3=' + (6+3) + ' Ваш ответ ' + primery1);  
+console.log('6-3=' + (6-3) + ' Ваш ответ ' + primery2);  
+console.log('6*3=' + (6*3) + ' Ваш ответ ' + primery3);  
+console.log('6/3=' + (6/3) + ' Ваш ответ ' + primery4);  
+console.log('6%3=' + (6%3) + ' Ваш ответ ' + primery5);
+alert('Код говно=) но Вы перейдите в консоль - F12')  
