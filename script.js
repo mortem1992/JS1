d507ce72e69693cddf5a2b39c7eb7419927824d9
Mortem <gurin.ivan@icloud.com>
2023-06-20 09:07:22 +0500

    JS add

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var userName = prompt('введите свое имя')
 var age = +prompt('введите свой возраст')
-var primery1 = +prompt('6+3=?')
-var primery2 = +prompt('6-3=?')
-var primery3 = +prompt('6*3=?')
-var primery4 = +prompt('6/3=?')
-var primery5 = +prompt('6%3=?')
+var primery1 = +prompt('73+37=?')
+var primery2 = +prompt('73-37=?')
+var primery3 = +prompt('81*3=?')
+var primery4 = +prompt('49/7=?')
+var primery5 = +prompt('13%3=?')
 console.log(userName);
 console.log(age)
-console.log('6+3=' + (6+3) + ' Ваш ответ ' + primery1);  
-console.log('6-3=' + (6-3) + ' Ваш ответ ' + primery2);  
-console.log('6*3=' + (6*3) + ' Ваш ответ ' + primery3);  
-console.log('6/3=' + (6/3) + ' Ваш ответ ' + primery4);  
-console.log('6%3=' + (6%3) + ' Ваш ответ ' + primery5);
+console.log('73+37=' + (73+37) + ' (Ваш ответ ' + primery1 + ')');  
+console.log('73-37=' + (73-37) + ' (Ваш ответ ' + primery2 + ')');  
+console.log('81*3=' + (81*3) + ' (Ваш ответ ' + primery3 + ')');  
+console.log('49/7=' + (49/7) + ' (Ваш ответ ' + primery4 + ')');  
+console.log('13%3=' + (13%3) + ' (Ваш ответ ' + primery5 + ')');
 alert('Код говно=) но Вы перейдите в консоль - F12')  
